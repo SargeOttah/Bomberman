0f801b813fb1f68ffdbf1d41f924d063a1dbcf12
SargeOttah <austeja.sumskyte@gmail.com>
2020-09-18 20:26:23 +0300

    Add project files.

---
+++ Bomberman/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bomberman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
