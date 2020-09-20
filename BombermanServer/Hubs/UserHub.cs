990732d466ff8745383bc76e6b34323cc796bfaa
Matas Žilinskas <zilinskas.matas1999@gmail.com>
2020-09-20 12:43:24 +0300

    SignalR Server Config

---
+++ BombermanServer/Hubs/UserHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace BombermanServer.Hubs
+{
+    public class UserHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
