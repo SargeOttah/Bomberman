819d428c85522d0b6ee79765346444363f6827b0
Matas Žilinskas <zilinskas.matas1999@gmail.com>
2020-11-18 20:12:24 +0200

    Ghost movement

--- BombermanServer/Services/Impl/EnemyMovementService.cs
+++ BombermanServer/Services/Impl/EnemyMovementService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using BombermanServer.Hubs;
 using Microsoft.AspNetCore.SignalR;
 using System.Linq;
 using System.Threading;
+using BombermanServer.Constants;
 
 namespace BombermanServer.Services.Impl
 {
@@ -21,24 +23,57 @@ namespace BombermanServer.Services.Impl
             _playerService = playerService;
 
             _mapService = mapService;
+            _mapService.LoadMap();
 
             UpdateGhostMovement();
         }
 
         public void UpdateGhostMovement()
         {
-            var x = 0;
-            var y = 480;
+            var x = 6.5f * MapConstants.tileSize;
+            var y = 5.5f * MapConstants.tileSize;
+            int? lastTurnIndex = null;
+            var rnd = new Random();
+
             new Timer(async _ =>
             {
-                var map = _mapService.GetMap();
-                foreach (var m in map)
-                {
-                    Console.WriteLine(m);
-                }
                 if (_playerService.GetPlayers().Any())
                 {
-                    x += 10;
+                    var allTurns = new List<bool>
+                    {
+                        !_mapService.IsObstacle(x - MapConstants.tileSize, y),
+                        !_mapService.IsObstacle(x, y - MapConstants.tileSize),
+                        !_mapService.IsObstacle(x + MapConstants.tileSize, y),
+                        !_mapService.IsObstacle(x, y + MapConstants.tileSize)
+                    };
+
+                    var availableTurnIndexes = new List<int>();
+                    for (var i = 0; i < allTurns.Count; i++)
+                    {
+                        if (allTurns[i]) availableTurnIndexes.Add(i);
+                    }
+
+                    if (lastTurnIndex is null || !allTurns[lastTurnIndex.Value])
+                    {
+                        lastTurnIndex = availableTurnIndexes[rnd.Next(availableTurnIndexes.Count)];
+                    }
+
+                    switch (lastTurnIndex)
+                    {
+                        case 0:
+                            x -= MapConstants.tileSize;
+                            break;
+                        case 1: 
+                            y -= MapConstants.tileSize;
+                            break;
+                        case 2: 
+                            x += MapConstants.tileSize;
+                            break;
+                        case 3:
+                            y += MapConstants.tileSize;
+                            break;
+                    }
+
                     await _hubContext.Clients.All.SendAsync("RefreshEnemies", x.ToString(), y.ToString());
                 }
             }, null, 0, 100);
